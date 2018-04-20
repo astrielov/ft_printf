440360d386a819982905bb9ac3603e1d55c031a1
Unknown <krendelllllll@gmail.com>
2018-04-20 15:05:25 +0300

    Fix unprintable char bug

--- src/handle/char.c
+++ src/handle/char.c
@@ -6,7 +6,7 @@
 /*   By: astrielov <astrielov@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 17:54:36 by astrielov         #+#    #+#             */
-/*   Updated: 2018/04/01 19:54:44 by astrelov         ###   ########.fr       */
+/*   Updated: 2018/04/20 15:02:55 by astrelov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void		char_post_padding(t_pf *arg, t_buff *arg_buff, size_t bytes)
 	}
 }
 
-void		fill_char(t_buff *arg_buff, unsigned int chr, size_t bytes)
+void		fill_wide_char(t_buff *arg_buff, unsigned int chr, size_t bytes)
 {
 	if (arg_buff->index + 4 >= arg_buff->size)
 		buff_realloc(arg_buff);
@@ -49,14 +49,24 @@ void		fill_char(t_buff *arg_buff, unsigned int chr, size_t bytes)
 		four_bytes(arg_buff, chr);
 }
 
+void		fill_char(t_buff *arg_buff, char chr)
+{
+	push_chars(arg_buff, chr, 1);
+}
+
 void		handle_char(t_pf *arg, t_buff *arg_buff, wchar_t chr)
 {
 	size_t	bytes;
 
-	bytes = wide_char_bytes(chr);
+	if (arg->specifier == 'c' && arg->length == LENGTH_LONG)
+		arg->specifier = 'C';
+	bytes = arg->specifier == 'c' ? 1 : wide_char_bytes(chr);
 	if (arg->specifier == 'c' || arg->specifier == 'C')
 		char_pre_padding(arg, arg_buff, bytes);
-	fill_char(arg_buff, (unsigned int)chr, bytes);
+	if (arg->specifier == 'c')
+		fill_char(arg_buff, (char) chr);
+	else
+		fill_wide_char(arg_buff, (unsigned int) chr, bytes);
 	if (arg->specifier == 'c' || arg->specifier == 'C')
 		char_post_padding(arg, arg_buff, bytes);
 }
